7b61d3a57f75b932565bceb2ada775dc88f8e048
Андрей Хахалин <draft-pick@yandex.ru>
2022-12-24 16:56:23 +0300

    9 commit

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -9,13 +9,13 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.pagination import (
-    LimitOffsetPagination,
-    PageNumberPagination,
-)
 from rest_framework.permissions import (
     IsAuthenticated,
     IsAuthenticatedOrReadOnly,
+)
+from rest_framework.pagination import (
+    LimitOffsetPagination,
+    PageNumberPagination,
 )
 from rest_framework import filters, viewsets
 from rest_framework import status
